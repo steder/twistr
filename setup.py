cec7ab711b3063fa885ca5a4419d10b43c7e2db9
Michael Steder <steder@gmail.com>
2019-06-14 16:44:26 -0500

    2.0: Upgrading to Python 3

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@
 from setuptools import setup
 
 
-setup(name='Twistr',
-      version='1.0.0',
-      description='Twistd wrapper that restarts twisted on code changes',
-      long_description=open("README.rst", "r").read(),
-      author='Mike Steder',
-      author_email='steder@gmail.com',
-      url='http://github.com/steder/twistr',
-      scripts=['bin/twistr'],
-      install_requires=["twisted"],
-      license="MIT",
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          ]
+setup(
+    name="Twistr",
+    version="2.0.0",
+    description="Twistd wrapper that restarts twisted on code changes",
+    long_description=open("README.rst", "r").read(),
+    author="Mike Steder",
+    author_email="steder@gmail.com",
+    url="http://github.com/steder/twistr",
+    scripts=["bin/twistr"],
+    install_requires=["twisted"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
 )
